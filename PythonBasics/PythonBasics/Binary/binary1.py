0c28c3b51705b787a20d8e6cf876c0d1a4de9211
FAREAST\v-sunaik <v-sunaik@microsoft.com>
2018-10-05 12:02:44 +0530

    python binary usage

---
+++ PythonBasics/PythonBasics/Binary/binary1.py
@@ -0,0 +1,7 @@
+#Printing binary numbers from 1 to 20
+
+for i in range(20):
+    print("{0:>2} in binary is {0:08b}".format(i))
+
+#Binary Shift left = Multiplies number by 2
+#Binary Shift right = Divides number by 2
