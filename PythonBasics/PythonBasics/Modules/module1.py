74df4bda6d3076cfa54f570495e34de98fa9ee83
FAREAST\v-sunaik <v-sunaik@microsoft.com>
2018-10-26 10:52:18 +0530

    python modules

---
+++ PythonBasics/PythonBasics/Modules/module1.py
@@ -0,0 +1,10 @@
+import turtle
+import time
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+
+turtle.circle(75)
+
+turtle.done()
---
+++ PythonBasics/PythonBasics/Modules/webBrowserModule.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+webbrowser.open("https://www.python.org")
+
+help(webbrowser)
